80411e7fd22ecd09cbdfb2bbf71c3ffecee5f6ea
Eric Liu <ericliu1602@gmail.com>
2024-12-18 22:42:35 -0500

    Solve day 17
    Also added "Journal" Section to `README.md`

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-My solutions for Advent of Code 2024, written in Rust for the learning experience.
+# Advent of Code 2024 Solutions
+## Author: Eric Liu
+
+Language: Rust
+
+## Journal
+
+### Day 5:
+Part 2 tripped me up here. I tried to create a topological sorting of all the pages without realizing that all the rules together actually created cycles. The solution was to only create a topological sort based off of the rules that applied to each set of pages, but this took a little bit of experimentation to figure out. Smarter people than me released proofs on the reddit that you can directly create a sort comparator based on the rules, which is quite interesting.
+
+### Day 6:
+Missed an edge case where two boxes could form a corner. This was a quick fix, but was difficult to find.
+
+### Day 9:
+This gave me tons of trouble for no reason, probably because it was my first time using structs and trying to maintain a freelist without plunging into the rabbit hole of linkedlists in Rust. My solution is honestly a little ugly, and is definitely suboptimal, but could be made optimal by changing a few implementation details and using a linkedlist. However, the actual runtime probably doesn't suffer that much because array copies can be pretty fast? It just won't scale as well as a linkedlist.
+
+### Day 16:
+Had tons of trouble with part 2. My initial idea was to maintain a Directed Acyclic Graph (DAG) of the fastest paths to each point so that I could trace the paths from the end back to the beginning and count the number of nodes passed. 
+However, I ran in to several problems, starting with inserting nodes into the DAG even if the paths weren't optimal.
+My next problem was that I did not know the directionality of the most optimal paths at the end, and had to pull this info out of my Dijkstra's implementation, which was a little ugly.
+In the middle of these problems, I attempted to code an implementation that unsuccessfully used a counter to keep track of the number of paths to each node, which did not correctly account for optimal paths.
+
+### Day 17:
+Did day 18 first because this one looked... hard...
+Part 1 was strangely cathartic after a semester of wrangling [gem5](https://github.com/gem5/gem5). I think I might port this architecture to verilog for laughs? Stay tuned.
+Part 2 made my brain feel massive. Unfortunately, my solution is _not_ general, otherwise Alan Turing would have some strong words for me (essentially solving the oracle problem?). I had the fortune of receiving a pretty well-behaved program, which led to a relatively straightforward-looking recursive backtracking solution.
+Of course, the process of getting to that solution was a long (but quite enjoyable!) one. I think this is the first time I have ever actually wanted to see my numbers in octal. Perhaps I'll have to look in to intcode from AoC2019?
+
+### Day 18:
+Another grid BFS! Yay...... (It feels like we've had quite a few of these this year)
+As it turns out, regex parsing is really slow! It took my code just as long to parse all the inputs as it did to solve part 2 with a suboptimal algorithm. 
+I'm going to have to come back to optimize. My current algorithm is O(n^2 * b) = O(n^4), where b is the number of bytes in the stream, and n is the length of the grid.
+I think this can be made linear in the number of bytes (quadratic for grid length?) via a union find and working backwards to merge connected components.
--- src/bin/day17.rs
+++ src/bin/day17.rs
@@ -1,33 +1,254 @@
+use core::panic;
+
 use aoc24::*;
+use regex::Regex;
 
 fn main() {
-    let binding = read_input(0);
+    let binding = read_input(17);
     let input = binding.as_str();
     println!("Part 1: {}", part1(&input));
+    println!("Part 1 fast: {}", part1fast(&input));
     println!("Part 2: {}", part2(&input));
 }
 
 fn part1(input: &str) -> String {
-    "".to_string()
+    let mut cpu = CPU::from_str(input);
+    cpu.execute(false);
+    cpu.output_str()
 }
 
+fn part1fast(input: &str) -> String {
+    let cpu = CPU::from_str(input);
+    cpu.simfast()
+}
+
+// Unfortunately, my solution will only work for my individual input, as a general analysis program
+// is NP.
+// This program just seeks to find an initial value for A which results in the following output:
+// 2417750317415530
 fn part2(input: &str) -> String {
-    "".to_string()
+    let cpu = CPU::from_str(input);
+    let imem = cpu.imem;
+    let a = backtrack(0, &imem, 15);
+    if a.is_some() {
+        return a.unwrap().to_string();
+    }
+    return "failed".to_string();
+}
+
+// Start from back of imem
+// on each iteration: check all 8 possibilities for digit corresponding to current insn
+// start with lowest possible value, since the prompt asks for the lowest value
+// for each possible digit: shift in to program, decrement index, make recursive call
+// if no paths possible, return None. otherwise, return the correct program.
+fn backtrack(program: u64, imem: &Vec<usize>, index: usize) -> Option<u64> {
+    for i in 0..8 {
+        let pushed = program << 3 | i;
+        if result(pushed) == imem[index] {
+            if index == 0 {
+                return Some(pushed);
+            }
+            let further = backtrack(pushed, imem, index - 1);
+            if further.is_some() {
+                return further;
+            }
+        }
+    }
+    None
+}
+
+fn result(a: u64) -> usize {
+    let last = a & 7;
+    let result = (a >> (last ^ 7)) ^ last;
+    (result & 7) as usize
+}
+
+struct CPU {
+    regs: [usize; 3],
+    imem: Vec<usize>,
+    pc: usize,
+    output: Vec<usize>,
+    insns_executed: usize,
+}
+
+impl CPU {
+    fn new(regs: [usize; 3], imem: Vec<usize>) -> Self {
+        CPU {
+            regs,
+            imem,
+            pc: 0,
+            output: Vec::new(),
+            insns_executed: 0,
+        }
+    }
+
+    fn from_str(input: &str) -> Self {
+        let sections = split_sections(input);
+        let re =
+            Regex::new(r"Register A: (?P<a>\d+)\nRegister B: (?P<b>\d+)\nRegister C: (?P<c>\d+)")
+                .unwrap();
+        let caps = re.captures(sections[0]).unwrap();
+        let a = caps.name("a").unwrap().as_str().parse::<usize>().unwrap();
+        let b = caps.name("b").unwrap().as_str().parse::<usize>().unwrap();
+        let c = caps.name("c").unwrap().as_str().parse::<usize>().unwrap();
+        let regs = [a, b, c];
+
+        let program_re = Regex::new(r"Program: (?P<insns>.*)\n?").unwrap();
+        let insn_input = program_re
+            .captures(sections[1])
+            .unwrap()
+            .name("insns")
+            .unwrap()
+            .as_str();
+        let insns = parse_row_major::<usize>(insn_input, ",")[0].clone();
+        Self::new(regs, insns)
+    }
+
+    fn simfast(&self) -> String {
+        let mut out = String::new();
+        let mut a: u64 = self.regs[0].clone() as u64;
+        while a > 0 {
+            let last = a & 7;
+            let result = ((a >> (last ^ 7)) ^ last) & 7;
+            out.push_str(result.to_string().as_str());
+            a >>= 3;
+        }
+        out
+    }
+
+    fn combo(&self, value: usize) -> usize {
+        match value {
+            0..=3 => value,
+            4 => self.regs[0],
+            5 => self.regs[1],
+            6 => self.regs[2],
+            _ => panic!(),
+        }
+    }
+
+    fn print_regfile(&self) {
+        println!(
+            "\tA: {:o} B: {:o} C: {:o}",
+            self.regs[0], self.regs[1], self.regs[2]
+        );
+    }
+
+    fn print_step(&self) {
+        const INSN_NAMES: [&str; 8] = ["adv", "bxl", "bst", "jnz", "bxc", "out", "bdv", "cdv"];
+        if self.pc >= self.imem.len() - 1 {
+            return;
+        }
+        println!(
+            "{}: {} {}",
+            self.pc,
+            INSN_NAMES[self.imem[self.pc]],
+            self.imem[self.pc + 1]
+        );
+    }
+
+    /**
+     * return false if halted
+     */
+    fn step(&mut self) -> bool {
+        if self.pc >= self.imem.len() - 1 {
+            return false;
+        }
+        let opcode = self.imem[self.pc];
+        let operand = self.imem[self.pc + 1];
+        let mut advance_pc = true;
+
+        match opcode {
+            0 => self.div(operand, 0),
+            1 => self.bxl(operand),
+            2 => self.bst(operand),
+            3 => advance_pc = self.jnz(operand),
+            4 => self.bxc(),
+            5 => self.out(operand),
+            6 => self.div(operand, 1),
+            7 => self.div(operand, 2),
+            _ => panic!(),
+        }
+
+        if advance_pc {
+            self.pc += 2;
+        }
+
+        true
+    }
+
+    fn execute(&mut self, verbose: bool) -> String {
+        if verbose {
+            self.print_regfile();
+            self.print_step();
+        }
+        while self.step() {
+            if verbose {
+                self.print_regfile();
+                self.print_step();
+            }
+        }
+        self.output_str()
+    }
+
+    // [dest] = A / (2 ** combo(op))
+    fn div(&mut self, operand: usize, dest: usize) {
+        self.regs[dest] = self.regs[0] >> self.combo(operand);
+    }
+
+    // B = combo(op) % 8
+    fn bst(&mut self, operand: usize) {
+        self.regs[1] = self.combo(operand) & 7;
+    }
+
+    // if A != 0 {PC = op}
+    // returns true if no jump
+    fn jnz(&mut self, operand: usize) -> bool {
+        if self.regs[0] == 0 {
+            return true;
+        }
+        self.pc = operand;
+        return false;
+    }
+
+    fn bxl(&mut self, operand: usize) {
+        self.regs[1] = self.regs[1] ^ operand;
+    }
+
+    fn bxc(&mut self) {
+        self.regs[1] = self.regs[1] ^ self.regs[2];
+    }
+
+    // pushes combo result modulo 8 to output
+    fn out(&mut self, operand: usize) {
+        self.output.push(self.combo(operand) & 7);
+    }
+
+    fn output_str(&self) -> String {
+        let mut ret = String::new();
+        if self.output.len() == 0 {
+            return ret;
+        }
+        for i in 0..(self.output.len() - 1) {
+            ret.push_str(self.output[i].to_string().as_str());
+            ret.push(',');
+        }
+        ret.push_str(self.output[self.output.len() - 1].to_string().as_str());
+        ret
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    static TEST_INPUT: &str = "";
+    static TEST_INPUT: &str = "Register A: 729
+Register B: 0
+Register C: 0
+
+Program: 0,1,5,4,3,0";
 
     #[test]
     fn test_part1() {
-        assert_eq!(part1(TEST_INPUT), "0");
-    }
-
-    #[test]
-    fn test_part2() {
-        assert_eq!(part2(TEST_INPUT), "0");
+        assert_eq!(part1(TEST_INPUT), "4,6,3,5,6,3,5,2,1,0");
     }
 }
